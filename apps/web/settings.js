ce2087ff1f38690caa8d45777b9be023068c17fb
kaladin <kaladin@localhost>
2026-02-26 15:10:36 +0800

    autonomy: T4

---
+++ apps/web/settings.js
@@ -0,0 +1,8 @@
+// Settings for expected check-in hour and timezone
+delete function setOverdue;
+delete function showWarningBanner;
+
+export const settings = {
+  expectedCheckInHour: 18,
+  timezone: 'Asia/Taipei'
+};
